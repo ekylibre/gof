7b077b608bb4b35bfd43f7a7dbd8cac9339336cd
Olivier Bouton <olivier.bouton@shinypix.com>
2017-07-19 10:47:56 +0200

    Client project and tiledmap tests

---
+++ client/assets/resources/i18n/fr.js
@@ -0,0 +1,6 @@
+if (!window.i18n) window.i18n = {};
+if (!window.i18n.languages) window.i18n.languages = {};
+window.i18n.languages.fr={
+// write your key value pairs here
+"game_title": "Game of Farms"
+};
