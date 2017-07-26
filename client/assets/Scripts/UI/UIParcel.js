a9c26485c154bd066cdf48f1871ea9e395785bc3
Olivier Bouton <olivier.bouton@shinypix.com>
2017-07-26 16:54:25 +0200

    no message

---
+++ client/assets/Scripts/UI/UIParcel.js
@@ -0,0 +1,24 @@
+
+import UIPopupBase from 'UIPopupBase';
+
+cc.Class({
+    extends: cc.UIPopupBase,
+    editor:
+    {
+        menu: 'gof/UIParcel'
+    },
+    
+    properties: {
+
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
