dbfaace855ba6c187ce7e93d0b78fa99938588ae
noobypapa <julien.castets@shinypix.com>
2017-07-31 14:19:50 +0200

    * Fixed: bad domain used in email from field

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -10,7 +10,7 @@ function Mailer() {
 
 Mailer.prototype.sendMail = function (recipient, subject, text, html) {
     var message = {
-        from: 'no-reply@game-of-farms.com',
+        from: 'no-reply@game-of-farms.ekylibre.com',
         to: recipient,
         subject: subject,
         text: text,
