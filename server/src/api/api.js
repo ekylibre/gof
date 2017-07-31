40514ac1bada6b21ed04643458e5baa2ab19e309
noobypapa <julien.castets@shinypix.com>
2017-07-31 17:47:28 +0200

    * Added API routes to GET plants, rotations, tools, equipments, additives

--- server/src/api/api.js
+++ server/src/api/api.js
@@ -6,9 +6,16 @@ const Hoek = require('hoek');
 const Bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const Plant = require('../models/plant');
 const User = require('../models/user');
+const Additive = require('../models/additive');
+const Equipment = require('../models/equipment');
+const Rotation = require('../models/rotation');
+const Tool = require('../models/tool');
 
 function Auth(server) {
+
+    var self = this;
 
     this.check = function(request, reply) {
         var email = request.auth.credentials.email;
@@ -22,7 +29,7 @@ function Auth(server) {
                     return reply(Boom.unauthorized());
                 }
 
-                reply({statusCode: 200, payload: { user: user}}).code(200);
+                reply({statusCode: 200, payload: { user: user}});
             }
         );
     };
@@ -59,16 +66,87 @@ function Auth(server) {
                                 expiresIn: '12h',
                             }
                         );
-                        return reply({statusCode: 200, payload: {accessToken: token}}).code(200);
+                        return reply({statusCode: 200, payload: {accessToken: token}});
                     }
                 );
             }
         );
     };
 
-    var CORS = false;
+    this.defaultGetModel = function(model, request, reply) {
+        
+        var id = request.params.id;
+        if(id) {
+            model.findById(id).exec(
+                (error, result) => {
+                    if(error) {
+                        return reply(Boom.serverUnavailable('Database error!', error));
+                    }
+                    if(!result) {
+                        return reply(Boom.notFound());
+                    }
+                    return reply(result);
+                }
+            );
+        } else {
+            var queryKeys = Object.keys(request.query);
+            if(queryKeys.length) {
+                var filter = {};
+                var fields = model.schema.paths;
+                queryKeys.forEach(function(k){
+                    if(fields[k]) {
+                        filter[k] = request.query[k];
+                    }
+                });
 
-    console.log(process.env.NODE_ENV);
+                model.find(filter).exec(
+                    (error, results) => {
+                        if(error) {
+                            return reply(Boom.serverUnavailable('Database error!', error));
+                        }
+                        if(!results) {
+                            return reply(Boom.notFound());
+                        }
+                        return reply(results);
+                    }
+                );
+            } else {
+                model.find().exec(
+                    (error, results) => {
+                        if(error) {
+                            return reply(Boom.serverUnavailable('Database error!', error));
+                        }
+                        if(!results) {
+                            return reply(Boom.notFound());
+                        }
+                        return reply(results);
+                    }
+                );
+            }
+        }
+    }
+
+    this.getPlants = function(request, reply) {
+        self.defaultGetModel(Plant, request, reply);
+    }
+
+    this.getTools = function(request, reply) {
+        self.defaultGetModel(Tool, request, reply);
+    }
+
+    this.getEquipments = function(request, reply) {
+        self.defaultGetModel(Equipment, request, reply);
+    }
+
+    this.getRotations = function(request, reply) {
+        self.defaultGetModel(Rotation, request, reply);
+    }
+
+    this.getAdditives = function(request, reply) {
+        self.defaultGetModel(Additive, request, reply);
+    }
+
+    var CORS = false;
 
     if(process.env.NODE_ENV === 'development') {
         //accept Cross Origin Resource Sharing when in dev
@@ -95,13 +173,60 @@ function Auth(server) {
             cors : CORS
         }
     });
+
+    server.route({
+        method:'GET',
+        path: '/api/plants/{id?}',
+        handler: this.getPlants/*,
+        config: {
+            auth : 'bearer',
+            cors : CORS
+        }*/
+    });
+
+    server.route({
+        method:'GET',
+        path: '/api/additives/{id?}',
+        handler: this.getAdditives/*,
+        config: {
+            auth : 'bearer',
+            cors : CORS
+        }*/
+    });
+
+    server.route({
+        method:'GET',
+        path: '/api/tools/{id?}',
+        handler: this.getTools/*,
+        config: {
+            auth : 'bearer',
+            cors : CORS
+        }*/
+    });
+
+    server.route({
+        method:'GET',
+        path: '/api/equipments/{id?}',
+        handler: this.getEquipments/*,
+        config: {
+            auth : 'bearer',
+            cors : CORS
+        }*/
+    });
+
+    server.route({
+        method:'GET',
+        path: '/api/rotations/{id?}',
+        handler: this.getRotations/*,
+        config: {
+            auth : 'bearer',
+            cors : CORS
+        }*/
+    });
 }
 
 function Api(server) {
-    
     this.Auth = new Auth(server);
-    
-
 }
 
 module.exports = Api;
