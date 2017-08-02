9175d817e6a2dc0fe6ff076763b789a2f5cded7d
noobypapa <julien.castets@shinypix.com>
2017-08-02 18:33:32 +0200

    * Removed DBmanager: no more needed since cli script exists to populate DB * Refactored Auth API service

--- server/src/api/api.js
+++ server/src/api/api.js
@@ -13,140 +13,9 @@ const Equipment = require('../models/equipment');
 const Rotation = require('../models/rotation');
 const Tool = require('../models/tool');
 
-function Auth(server) {
-
-    var self = this;
-
-    this.check = function(request, reply) {
-        var email = request.auth.credentials.email;
-        User.findOne( {email: email}).select('-password -resetpasswordtoken').exec(
-            (error, user) => {
-                if(error) {
-                    return reply(Boom.badRequest());
-                }
-                
-                if(!user) {
-                    return reply(Boom.unauthorized());
-                }
-
-                reply({statusCode: 200, payload: { user: user}});
-            }
-        );
-    };
-
-    this.login = function(request, reply) {
-        var email = request.payload.email;
-        var password = request.payload.password;
-
-        User.findOne({email:email},
-            (error, user) => {
-                if(error) {
-                    return reply(Boom.badRequest());
-                }
-
-                if(!user) {
-                    return reply(Boom.unauthorized());
-                }
-
-                Bcrypt.compare(password, user.password, 
-                    (error, same) => {
-                        if(error) {
-                            return reply(Boom.internal());
-                        }
-                        if(!same) {
-                            return reply(Boom.unauthorized());
-                        }
-                        const token = jwt.sign({
-                                email: user.email,
-                                firstname: user.firstName
-                            }, 
-                            config.get('Jwt.key'),
-                            {
-                                algorithm: 'HS256',
-                                expiresIn: '12h',
-                            }
-                        );
-                        return reply({statusCode: 200, payload: {accessToken: token}});
-                    }
-                );
-            }
-        );
-    };
-
-    this.defaultGetModel = function(model, request, reply) {
-        
-        var id = request.params.id;
-        if(id) {
-            model.findById(id).exec(
-                (error, result) => {
-                    if(error) {
-                        return reply(Boom.serverUnavailable('Database error!', error));
-                    }
-                    if(!result) {
-                        return reply(Boom.notFound());
-                    }
-                    return reply(result);
-                }
-            );
-        } else {
-            var queryKeys = Object.keys(request.query);
-            if(queryKeys.length) {
-                var filter = {};
-                var fields = model.schema.paths;
-                queryKeys.forEach(function(k){
-                    if(fields[k]) {
-                        filter[k] = request.query[k];
-                    }
-                });
-
-                model.find(filter).exec(
-                    (error, results) => {
-                        if(error) {
-                            return reply(Boom.serverUnavailable('Database error!', error));
-                        }
-                        if(!results) {
-                            return reply(Boom.notFound());
-                        }
-                        return reply(results);
-                    }
-                );
-            } else {
-                model.find().exec(
-                    (error, results) => {
-                        if(error) {
-                            return reply(Boom.serverUnavailable('Database error!', error));
-                        }
-                        if(!results) {
-                            return reply(Boom.notFound());
-                        }
-                        return reply(results);
-                    }
-                );
-            }
-        }
-    }
-
-    this.getPlants = function(request, reply) {
-        self.defaultGetModel(Plant, request, reply);
-    }
-
-    this.getTools = function(request, reply) {
-        self.defaultGetModel(Tool, request, reply);
-    }
-
-    this.getEquipments = function(request, reply) {
-        self.defaultGetModel(Equipment, request, reply);
-    }
-
-    this.getRotations = function(request, reply) {
-        self.defaultGetModel(Rotation, request, reply);
-    }
-
-    this.getAdditives = function(request, reply) {
-        self.defaultGetModel(Additive, request, reply);
-    }
-
+var Auth = function(server) {
     var CORS = false;
+    var self = this;
 
     if(process.env.NODE_ENV === 'development') {
         //accept Cross Origin Resource Sharing when in dev
@@ -158,7 +27,7 @@ function Auth(server) {
     server.route({
         method: 'GET',
         path: '/api/auth/check',
-        handler: this.check,
+        handler: function(request, reply) { self.check(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
@@ -168,7 +37,7 @@ function Auth(server) {
     server.route({
         method: 'POST',
         path: '/api/auth/login',
-        handler: this.login,
+        handler: function(request, reply) { self.login(request, reply); },
         config: {
             cors : CORS
         }
@@ -177,7 +46,7 @@ function Auth(server) {
     server.route({
         method:'GET',
         path: '/api/plants/{id?}',
-        handler: this.getPlants,
+        handler: function(request, reply) { self.getPlants(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
@@ -187,7 +56,7 @@ function Auth(server) {
     server.route({
         method:'GET',
         path: '/api/additives/{id?}',
-        handler: this.getAdditives,
+        handler: function(request, reply) { self.getAdditives(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
@@ -197,7 +66,7 @@ function Auth(server) {
     server.route({
         method:'GET',
         path: '/api/tools/{id?}',
-        handler: this.getTools,
+        handler: function(request, reply) { self.getTools(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
@@ -207,7 +76,7 @@ function Auth(server) {
     server.route({
         method:'GET',
         path: '/api/equipments/{id?}',
-        handler: this.getEquipments,
+        handler: function(request, reply) { self.getEquipments(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
@@ -217,12 +86,132 @@ function Auth(server) {
     server.route({
         method:'GET',
         path: '/api/rotations/{id?}',
-        handler: this.getRotations,
+        handler: function(request, reply) { self.getRotations(request, reply); },
         config: {
             auth : 'bearer',
             cors : CORS
         }
     });
+}
+
+Auth.prototype.check = function(request, reply) {
+    var email = request.auth.credentials.email;
+    User.findOne( {email: email}).select('-password -resetpasswordtoken').exec(
+        (error, user) => {
+            if(error) {
+                return reply(Boom.badRequest());
+            }
+            
+            if(!user) {
+                return reply(Boom.unauthorized());
+            }
+
+            reply({statusCode: 200, payload: { user: user}});
+        }
+    );
+};
+
+Auth.prototype.login = function(request, reply) {
+    var email = request.payload.email;
+    var password = request.payload.password;
+
+    User.findOne({email:email}, (error, user) => {
+        if(error) {
+            return reply(Boom.badRequest());
+        }
+
+        if(!user) {
+            return reply(Boom.unauthorized());
+        }
+
+        Bcrypt.compare(password, user.password, (error, same) => {
+            if(error) {
+                return reply(Boom.internal());
+            }
+            if(!same) {
+                return reply(Boom.unauthorized());
+            }
+            const token = jwt.sign({
+                    email: user.email,
+                    firstname: user.firstName
+                }, 
+                config.get('Jwt.key'),
+                {
+                    algorithm: 'HS256',
+                    expiresIn: '12h',
+                }
+            );
+            return reply({statusCode: 200, payload: {accessToken: token}});
+        });
+    });
+};
+
+Auth.prototype.defaultGetModel = function(model, request, reply) {
+    var id = request.params.id;
+    if(id) {
+        model.findById(id).exec(
+            (error, result) => {
+                if(error) {
+                    return reply(Boom.serverUnavailable('Database error!', error));
+                }
+                if(!result) {
+                    return reply(Boom.notFound());
+                }
+                return reply(result);
+            }
+        );
+    } else {
+        var queryKeys = Object.keys(request.query);
+        if(queryKeys.length) {
+            var filter = {};
+            var fields = model.schema.paths;
+            queryKeys.forEach(function(k) {
+                if(fields[k]) {
+                    filter[k] = request.query[k];
+                }
+            });
+
+            model.find(filter).exec((error, results) => {
+                if(error) {
+                    return reply(Boom.serverUnavailable('Database error!', error));
+                }
+                if(!results) {
+                    return reply(Boom.notFound());
+                }
+                return reply(results);
+            });
+        } else {
+            model.find().exec((error, results) => {
+                if(error) {
+                    return reply(Boom.serverUnavailable('Database error!', error));
+                }
+                if(!results) {
+                    return reply(Boom.notFound());
+                }
+                return reply(results);
+            });
+        }
+    }
+}
+
+Auth.prototype.getPlants = function(request, reply) {
+    this.defaultGetModel(Plant, request, reply);
+}
+
+Auth.prototype.getTools = function(request, reply) {
+    this.defaultGetModel(Tool, request, reply);
+}
+
+Auth.prototype.getEquipments = function(request, reply) {
+    this.defaultGetModel(Equipment, request, reply);
+}
+
+Auth.prototype.getRotations = function(request, reply) {
+    this.defaultGetModel(Rotation, request, reply);
+}
+
+Auth.prototype.getAdditives = function(request, reply) {
+    this.defaultGetModel(Additive, request, reply);
 }
 
 function Api(server) {
